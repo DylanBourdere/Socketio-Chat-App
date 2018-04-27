042138cf8dd79c00e82321ead048b03f1e78d613
Loperiane <loperanes@gmail.com>
2018-04-27 20:35:08 +0200

    Finally manage to install bootstrap vue correctly

---
+++ chat/src/components/Chat.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+      <b-alert show variant="primary">Primary Alert</b-alert>
+  <b-alert show variant="secondary">Secondary Alert</b-alert>
+  <b-alert show variant="success">Success Alert</b-alert>
+  <b-alert show variant="danger">Danger Alert</b-alert>
+  <b-alert show variant="warning">Warning Alert</b-alert>
+  <b-alert show variant="info">Info Alert</b-alert>
+  <b-alert show variant="light">Light Alert</b-alert>
+  <b-alert show variant="dark">Dark Alert</b-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chat',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
